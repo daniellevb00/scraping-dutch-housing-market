e467d72224f803ccbd189624533402fe2ad6d99c
Daniëlle van Bruggen <d.m.vanbruggen@tilburguniversity.edu>
2021-10-05 19:30:27 +0200

    fixed thousand/decimal delimiters scraped data for working dataset in R

--- src/collection/Exploring_scraped_data.R
+++ src/collection/Exploring_scraped_data.R
@@ -4,7 +4,7 @@
 ##loading the exported data from Jupyter Notebook
 library(readr)
 library(dplyr)
-Huizenzoeker_NB <- read_csv("Huizenzoeker_NB_data.csv")
+Huizenzoeker_NB <- read_csv("Huizenzoeker_NB_data1.csv")
 View(Huizenzoeker_NB)
 
 #save as new dataset
@@ -14,7 +14,7 @@ summary(huizen_NB)
 head(huizen_NB)
 
 #PROBLEM: Huizenzoeker.nl uses '.' to indicate both decimals and thousand limiters; so for R it isn't clear whether its 2000 or 2.00 e.g. 
-#(we need to fix this still!!)
+#(PROBLEM FIXED :), I changed the thousands delimiters in Python to , and in R this removes the thousand delimiter, I dont know if we should add it for clarity? but this already returns better summary statistics I think)
 
 ##checking classes variables
 #correcting the classes of the variables
@@ -22,12 +22,12 @@ lapply(huizen_NB, class) #initially they are all seen as characters, so we need 
 #QUADRANT 1
 #gemiddelde vraagprijs 
 huizen_NB$gem_vraagprijs<-huizen_NB$'Gem. vraagprijs'
-huizen_NB$gem_vraagprijs<-gsub('[£á]','', huizen_NB$gem_vraagprijs) #remove euro signs
+huizen_NB$gem_vraagprijs<-gsub('[???]','', huizen_NB$gem_vraagprijs) #remove euro signs
 huizen_NB$gem_vraagprijs<-parse_number(huizen_NB$gem_vraagprijs) #PROBLEM: 1 average above 1 million, is seen as 2000 instead of 2,000,000!! 
 huizen_NB$gem_vraagprijs<-as.numeric(huizen_NB$gem_vraagprijs)
 #verandering 
-huizen_NB$perc_ver_vraagprijs<-huizen_NB$'%£G Vraagprijs (t.o.v vorige maand)'
-huizen_NB$perc_ver_vraagprijs<-gsub('[£á]','',huizen_NB$perc_ver_vraagprijs)
+huizen_NB$perc_ver_vraagprijs<-huizen_NB$'%?? Vraagprijs (t.o.v vorige maand)'
+huizen_NB$perc_ver_vraagprijs<-gsub('[???]','',huizen_NB$perc_ver_vraagprijs)
 huizen_NB$perc_ver_vraagprijs<-parse_number(huizen_NB$perc_ver_vraagprijs)
 huizen_NB$perc_ver_vraagprijs<-as.numeric(huizen_NB$perc_ver_vraagprijs)
 
@@ -36,18 +36,18 @@ huizen_NB$perc_ver_vraagprijs<-as.numeric(huizen_NB$perc_ver_vraagprijs)
 huizen_NB$verk_woningen<-huizen_NB$'Verkochte woningen'
 huizen_NB$verk_woningen<-as.numeric(huizen_NB$verk_woningen)
 #verandering
-huizen_NB$perc_ver_verkocht<-huizen_NB$'%£G Verkocht (t.o.v vorige maand)'
+huizen_NB$perc_ver_verkocht<-huizen_NB$'%?? Verkocht (t.o.v vorige maand)'
 huizen_NB$perc_ver_verkocht<-gsub('[%]','',huizen_NB$perc_ver_verkocht)
 huizen_NB$perc_ver_verkocht<-parse_number(huizen_NB$perc_ver_verkocht)
 huizen_NB$perc_ver_verkocht<-as.numeric(huizen_NB$perc_ver_verkocht)
 
 #QUADRANT 3
 #gemiddelde m2 prijs 
-huizen_NB$gem_m2prijs<-gsub('[£á]','', huizen_NB$`Gem. m2 prijs`) #remove euro signs
+huizen_NB$gem_m2prijs<-gsub('[???]','', huizen_NB$`Gem. m2 prijs`) #remove euro signs
 huizen_NB$gem_m2prijs<-parse_number(huizen_NB$gem_m2prijs)
 huizen_NB$gem_m2prijs<-as.numeric(huizen_NB$gem_m2prijs)
 #verandering
-huizen_NB$perc_ver_m2prijs<-huizen_NB$'%£G M2 prijs (t.o.v vorige maand)'
+huizen_NB$perc_ver_m2prijs<-huizen_NB$'%?? M2 prijs (t.o.v vorige maand)'
 huizen_NB$perc_ver_m2prijs<-gsub('[%]','',huizen_NB$perc_ver_m2prijs)
 huizen_NB$perc_ver_m2prijs<-parse_number(huizen_NB$perc_ver_m2prijs)
 huizen_NB$perc_ver_m2prijs<-as.numeric(huizen_NB$perc_ver_m2prijs)
@@ -58,13 +58,13 @@ huizen_NB$perc_overboden<-gsub('[%]','', huizen_NB$`% Vraagprijs overboden`)
 huizen_NB$perc_overboden<-parse_number(huizen_NB$perc_overboden)
 huizen_NB$perc_overboden<-as.numeric(huizen_NB$perc_overboden)
 #verandering
-huizen_NB$perc_ver_overboden<-huizen_NB$'%£G Overboden (t.o.v vorige maand)'
+huizen_NB$perc_ver_overboden<-huizen_NB$'%?? Overboden (t.o.v vorige maand)'
 huizen_NB$perc_ver_overboden<-gsub('[%]','', huizen_NB$perc_ver_overboden) 
 huizen_NB$perc_ver_overboden<-parse_number(huizen_NB$perc_ver_overboden)
 huizen_NB$perc_ver_overboden<-as.numeric(huizen_NB$perc_ver_overboden)
 
 #BESTEEDBAAR INKOMEN
-huizen_NB$best_inkomen<-gsub('[£á]','', huizen_NB$'Besteedbaar inkomen (per huishouden)') #remove euro signs
+huizen_NB$best_inkomen<-gsub('[???]','', huizen_NB$'Besteedbaar inkomen (per huishouden)') #remove euro signs
 huizen_NB$best_inkomen<-parse_number(huizen_NB$best_inkomen)
 huizen_NB$best_inkomen<-as.numeric(huizen_NB$best_inkomen)
 
@@ -72,12 +72,13 @@ huizen_NB$best_inkomen<-as.numeric(huizen_NB$best_inkomen)
 #add code if finished
 
 #now we remove the old columns that we don't need anymore
-huizen_NB<-huizen_NB%>%select(-c('Gem. vraagprijs','%£G Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%£G Verkocht (t.o.v vorige maand)','Gem. m2 prijs','%£G M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%£G Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)'))
+huizen_NB1<-huizen_NB%>%select(-c('Gem. vraagprijs','%?? Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%?? Verkocht (t.o.v vorige maand)','Gem. m2 prijs','%?? M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%?? Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)'))
 
 #now we can explore the summary statistics for all our variables for the province Noord-Brabant
-summary(huizen_NB)
+View(huizen_NB1)
+summary(huizen_NB1)
 #now convert the dataframe to a CSV file to import it into Jupyter to there see the summary statistics too
-write.csv(huizen_NB, "C:\\Users\\danie\\OneDrive\\Documents\\Repositories\\oDCM-project-team-3\\src\\collection\\huizen_NB.csv",row.names=FALSE)
+write.csv(huizen_NB1, "C:\\Users\\danie\\OneDrive\\Documents\\Repositories\\oDCM-project-team-3\\src\\collection\\huizen_NB1.csv",row.names=FALSE)
 
 ##Now we can use this dataset to visualise our scraped data and create graphs if we want. 
 
