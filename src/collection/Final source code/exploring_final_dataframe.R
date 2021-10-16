87f31a88bfb47da66f40df48808e1f2caba17727
Lesley Haerkens <lesleyhaerkens@air-van-lesley.home>
2021-10-17 01:28:16 +0200

    created RMarkdown, wrote code for data on street-level, created plot

--- src/collection/Final source code/exploring_final_dataframe.R
+++ src/collection/Final source code/exploring_final_dataframe.R
@@ -6,10 +6,11 @@ setwd("C:/Users/danie/OneDrive/Documents/Repositories/oDCM-project-team-3/src/co
 library(readr)
 library(dplyr)
 huizenzoeker <- read_csv("huizenzoeker_scraper_data.csv")
+huizenzoeker <- huizenzoeker_scraper_data
 View(huizenzoeker)
 summary(huizenzoeker)
 head(huizenzoeker)
-huizenzoeker<-huizenzoeker%>%select(-'...1') #delete the first row with indices (not needed)
+huizenzoeker<-huizenzoeker[2:15] #delete the first row with indices (not needed)
 #UPDATE: I changed the thousands delimiters in Python to , and in R this removes the thousand delimiter, I dont know if we should add it for clarity? but this already returns better summary statistics I think)
 #WARNING: the euro signs and delta's may change due to encoding erorrs when saving the file; if this happens, just replace the wierd signs with either the euro sign or the delta and youre fine
 #Therefore try to save the file with Big5 encoding (then there likely isn't an error)
@@ -20,12 +21,12 @@ lapply(huizenzoeker, class) #initially they are all seen as characters, so we ne
 #QUADRANT 1
 #gemiddelde vraagprijs 
 huizenzoeker$gem_vraagprijs<-huizenzoeker$'Gem. vraagprijs'
-huizenzoeker$gem_vraagprijs<-gsub('[£á]','', huizenzoeker$gem_vraagprijs) #remove euro signs
-huizenzoeker$gem_vraagprijs<-parse_number(huizenzoeker$gem_vraagprijs) #PROBLEM: 1 average above 1 million, is seen as 2000 instead of 2,000,000!! 
+huizenzoeker$gem_vraagprijs<-gsub('[â‚¬]','', huizenzoeker$gem_vraagprijs)
+huizenzoeker$gem_vraagprijs<-parse_number(huizenzoeker$gem_vraagprijs) 
 huizenzoeker$gem_vraagprijs<-as.numeric(huizenzoeker$gem_vraagprijs)
 #verandering 
-huizenzoeker$perc_ver_vraagprijs<-huizenzoeker$'%£G Vraagprijs (t.o.v vorige maand)'
-huizenzoeker$perc_ver_vraagprijs<-gsub('[£á]','',huizenzoeker$perc_ver_vraagprijs)
+huizenzoeker$perc_ver_vraagprijs<-huizenzoeker$'%Î” Vraagprijs (t.o.v vorige maand)'
+huizenzoeker$perc_ver_vraagprijs<-gsub('[%]','',huizenzoeker$perc_ver_vraagprijs)
 huizenzoeker$perc_ver_vraagprijs<-parse_number(huizenzoeker$perc_ver_vraagprijs)
 huizenzoeker$perc_ver_vraagprijs<-as.numeric(huizenzoeker$perc_ver_vraagprijs)
 
@@ -34,18 +35,18 @@ huizenzoeker$perc_ver_vraagprijs<-as.numeric(huizenzoeker$perc_ver_vraagprijs)
 huizenzoeker$verk_woningen<-huizenzoeker$'Verkochte woningen'
 huizenzoeker$verk_woningen<-as.numeric(huizenzoeker$verk_woningen)
 #verandering
-huizenzoeker$perc_ver_verkocht<-huizenzoeker$'%£G Verkochte woningen (t.o.v vorige maand)'
+huizenzoeker$perc_ver_verkocht<-huizenzoeker$'%Î” Verkochte woningen (t.o.v vorige maand)'
 huizenzoeker$perc_ver_verkocht<-gsub('[%]','',huizenzoeker$perc_ver_verkocht)
 huizenzoeker$perc_ver_verkocht<-parse_number(huizenzoeker$perc_ver_verkocht)
 huizenzoeker$perc_ver_verkocht<-as.numeric(huizenzoeker$perc_ver_verkocht)
 
 #QUADRANT 3
 #gemiddelde m2 prijs 
-huizenzoeker$gem_m2prijs<-gsub('[£á]','', huizenzoeker$`Gem. m2 prijs`) #remove euro signs
+huizenzoeker$gem_m2prijs<-gsub('[â‚¬]','', huizenzoeker$`Gem. m2 prijs`) #remove euro signs
 huizenzoeker$gem_m2prijs<-parse_number(huizenzoeker$gem_m2prijs)
 huizenzoeker$gem_m2prijs<-as.numeric(huizenzoeker$gem_m2prijs)
 #verandering
-huizenzoeker$perc_ver_m2prijs<-huizenzoeker$'%£G M2 prijs (t.o.v vorige maand)'
+huizenzoeker$perc_ver_m2prijs<-huizenzoeker$'%Î” M2 prijs (t.o.v vorige maand)'
 huizenzoeker$perc_ver_m2prijs<-gsub('[%]','',huizenzoeker$perc_ver_m2prijs)
 huizenzoeker$perc_ver_m2prijs<-parse_number(huizenzoeker$perc_ver_m2prijs)
 huizenzoeker$perc_ver_m2prijs<-as.numeric(huizenzoeker$perc_ver_m2prijs)
@@ -56,13 +57,13 @@ huizenzoeker$perc_overboden<-gsub('[%]','', huizenzoeker$`% Vraagprijs overboden
 huizenzoeker$perc_overboden<-parse_number(huizenzoeker$perc_overboden)
 huizenzoeker$perc_overboden<-as.numeric(huizenzoeker$perc_overboden)
 #verandering
-huizenzoeker$perc_ver_overboden<-huizenzoeker$'%£G Overboden (t.o.v vorige maand)'
+huizenzoeker$perc_ver_overboden<-huizenzoeker$'%Î” Overboden (t.o.v vorige maand)'
 huizenzoeker$perc_ver_overboden<-gsub('[%]','', huizenzoeker$perc_ver_overboden) 
 huizenzoeker$perc_ver_overboden<-parse_number(huizenzoeker$perc_ver_overboden)
 huizenzoeker$perc_ver_overboden<-as.numeric(huizenzoeker$perc_ver_overboden)
 
 #BESTEEDBAAR INKOMEN
-huizenzoeker$best_inkomen<-gsub('[£á]','', huizenzoeker$'Besteedbaar inkomen (per huishouden)') #remove euro signs
+huizenzoeker$best_inkomen<-gsub('[â‚¬ ]','', huizenzoeker$'Besteedbaar inkomen (per huishouden)') #remove euro signs
 huizenzoeker$best_inkomen<-parse_number(huizenzoeker$best_inkomen) #UPDATE: HERE NOW RETURNS ERROR: 3 PARSING FAILURES: because here are NA's
 huizenzoeker$best_inkomen<-as.numeric(huizenzoeker$best_inkomen)
 
@@ -80,7 +81,7 @@ huizenzoeker$perc_pop_daling<-parse_number(huizenzoeker$perc_pop_daling)
 huizenzoeker$perc_pop_daling<-as.numeric(huizenzoeker$perc_pop_daling)
 
 #now we remove the old columns that we don't need anymore
-huizenzoeker_data2<-huizenzoeker%>%select(-c('Gem. vraagprijs','%£G Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%£G Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%£G M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%£G Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', '% Populatie stijging','% Populatie daling', 'Aantal inwoners'))
+huizenzoeker_data2<-huizenzoeker%>%select(-c('Gem. vraagprijs','%Î” Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%Î” Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%Î” M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%Î” Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', '% Populatie stijging','% Populatie daling', 'Aantal inwoners'))
 
 #now we can explore the summary statistics for all our variables for the province Noord-Brabant
 View(huizenzoeker_data2)
@@ -92,10 +93,59 @@ summary(huizenzoeker_data2)
 #now convert the dataframe to a CSV file to import it into Jupyter to there see the summary statistics too
 write.csv(huizenzoeker_data2, "C:\\Users\\danie\\OneDrive\\Documents\\Repositories\\oDCM-project-team-3\\src\\collection\\huizenzoeker_data2.csv",row.names=FALSE)
 
+
+##### Provinces
 ##Now we can use this dataset to visualise our scraped data and create graphs if we want. 
+library(ggplot2)
+library(maps)
+library(viridis)
+library(tibble) 
+library(sp)
+library(extrafont)
 
+NL <- map_data("world", region = "Netherlands")
+ggplot(NL, aes(x = long, y = lat)) +
+  geom_polygon(aes( group = group, fill = region))+
+  theme(legend.position = "none")
 
+NLD <- readRDS("gadm36_NLD_1_sp (1).rds")
+NLD_data <- NLD@data
+plot(NLD)
 
+ggplot(data = NLD, aes(x = long, y = lat))+
+  geom_polygon( ) +
+  coord_fixed()
+ggplot()+
+  geom_polygon(data = NLD, aes(x = long, y = lat, group = group)) +
+  coord_map()
 
+NLD_fixed <- subset(NLD, !NLD$NAME_1  %in% c("Veldhoven"))
+NLD_f<- fortify(NLD)
+
+names_and_numbers <- data_frame(id=rownames(NLD_data),
+                                Province=NLD_data$NAME_1) %>% 
+  left_join(huizenzoeker_province_data1, by = "Province")
+
+final_map <- left_join(NLD_f, names_and_numbers, by = "id") 
+
+ggplot(final_map) +
+  theme_minimal() +
+  geom_polygon(aes(x = long, y = lat, group = group, fill= perc_overboden)) +
+  labs(x="", y="", title="Dutch Housing Crisis: Which Areas Are Most Outbid?",
+       subtitle = "Concerning Provinces in the Netherlands") +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.position = "bottom") +
+  coord_map()+
+  scale_fill_distiller(name = "% outbid", 
+                       palette = "Reds",
+                       direction = 1) +
+  theme(legend.position = "right",
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank()) 
 
 
--- src/collection/Final source code/exploring_province_dataframe.R
+++ src/collection/Final source code/exploring_province_dataframe.R
@@ -6,10 +6,11 @@ setwd("C:/Users/danie/OneDrive/Documents/Repositories/ODCM-project-team-3/src/co
 library(readr)
 library(dplyr)
 huizenzoeker1 <- read_csv("huizenzoeker_province_data.csv")
+huizenzoeker1 <- huizenzoeker_province_data
 View(huizenzoeker1)
 summary(huizenzoeker1)
 head(huizenzoeker1)
-huizenzoeker1<-huizenzoeker1%>%select(-'...1') #delete the first row with indices (not needed)
+huizenzoeker1<-huizenzoeker1[2:13] #delete the first row with indices (not needed)
 #UPDATE: I changed the thousands delimiters in Python to , and in R this removes the thousand delimiter, I dont know if we should add it for clarity? but this already returns better summary statistics I think)
 #SAME WARNING HERE: SAVE WITH BIG5 ENCODING SUCH THAT EURO SIGNS AND DELTA'S DON'T GET MIXED UP
 #REMARKABLE: %POPULATIE DALING IS EMPTY, AS FOR EVERY PROVINCE THE NUMBER OF INHABITANTS HAS INCREASED (so we can get rid of this column)
@@ -21,12 +22,12 @@ lapply(huizenzoeker1, class) #initially they are all seen as characters, so we n
 #QUADRANT 1
 #gemiddelde vraagprijs 
 huizenzoeker1$gem_vraagprijs<-huizenzoeker1$'Gem. vraagprijs'
-huizenzoeker1$gem_vraagprijs<-gsub('[£á]','', huizenzoeker1$gem_vraagprijs) #remove euro signs
+huizenzoeker1$gem_vraagprijs<-gsub('[â‚¬]','', huizenzoeker1$gem_vraagprijs) #remove euro signs
 huizenzoeker1$gem_vraagprijs<-parse_number(huizenzoeker1$gem_vraagprijs) #PROBLEM: 1 average above 1 million, is seen as 2000 instead of 2,000,000!! 
 huizenzoeker1$gem_vraagprijs<-as.numeric(huizenzoeker1$gem_vraagprijs)
 #verandering 
-huizenzoeker1$perc_ver_vraagprijs<-huizenzoeker1$'%£G Vraagprijs (t.o.v vorige maand)'
-huizenzoeker1$perc_ver_vraagprijs<-gsub('[£á]','',huizenzoeker1$perc_ver_vraagprijs)
+huizenzoeker1$perc_ver_vraagprijs<-huizenzoeker1$'%Î” Vraagprijs (t.o.v vorige maand)'
+huizenzoeker1$perc_ver_vraagprijs<-gsub('[%]','',huizenzoeker1$perc_ver_vraagprijs)
 huizenzoeker1$perc_ver_vraagprijs<-parse_number(huizenzoeker1$perc_ver_vraagprijs)
 huizenzoeker1$perc_ver_vraagprijs<-as.numeric(huizenzoeker1$perc_ver_vraagprijs)
 
@@ -35,18 +36,18 @@ huizenzoeker1$perc_ver_vraagprijs<-as.numeric(huizenzoeker1$perc_ver_vraagprijs)
 huizenzoeker1$verk_woningen<-huizenzoeker1$'Verkochte woningen'
 huizenzoeker1$verk_woningen<-as.numeric(huizenzoeker1$verk_woningen)
 #verandering
-huizenzoeker1$perc_ver_verkocht<-huizenzoeker1$'%£G Verkochte woningen (t.o.v vorige maand)'
+huizenzoeker1$perc_ver_verkocht<-huizenzoeker1$'%Î” Verkochte woningen (t.o.v vorige maand)'
 huizenzoeker1$perc_ver_verkocht<-gsub('[%]','',huizenzoeker1$perc_ver_verkocht)
 huizenzoeker1$perc_ver_verkocht<-parse_number(huizenzoeker1$perc_ver_verkocht)
 huizenzoeker1$perc_ver_verkocht<-as.numeric(huizenzoeker1$perc_ver_verkocht)
 
 #QUADRANT 3
 #gemiddelde m2 prijs 
-huizenzoeker1$gem_m2prijs<-gsub('[£á]','', huizenzoeker1$`Gem. m2 prijs`) #remove euro signs
+huizenzoeker1$gem_m2prijs<-gsub('[â‚¬]','', huizenzoeker1$`Gem. m2 prijs`) #remove euro signs
 huizenzoeker1$gem_m2prijs<-parse_number(huizenzoeker1$gem_m2prijs)
 huizenzoeker1$gem_m2prijs<-as.numeric(huizenzoeker1$gem_m2prijs)
 #verandering
-huizenzoeker1$perc_ver_m2prijs<-huizenzoeker1$'%£G M2 prijs (t.o.v vorige maand)'
+huizenzoeker1$perc_ver_m2prijs<-huizenzoeker1$'%Î” M2 prijs (t.o.v vorige maand)'
 huizenzoeker1$perc_ver_m2prijs<-gsub('[%]','',huizenzoeker1$perc_ver_m2prijs)
 huizenzoeker1$perc_ver_m2prijs<-parse_number(huizenzoeker1$perc_ver_m2prijs)
 huizenzoeker1$perc_ver_m2prijs<-as.numeric(huizenzoeker1$perc_ver_m2prijs)
@@ -57,13 +58,13 @@ huizenzoeker1$perc_overboden<-gsub('[%]','', huizenzoeker1$`% Vraagprijs overbod
 huizenzoeker1$perc_overboden<-parse_number(huizenzoeker1$perc_overboden)
 huizenzoeker1$perc_overboden<-as.numeric(huizenzoeker1$perc_overboden)
 #verandering
-huizenzoeker1$perc_ver_overboden<-huizenzoeker1$'%£G Overboden (t.o.v vorige maand)'
+huizenzoeker1$perc_ver_overboden<-huizenzoeker1$'%Î” Overboden (t.o.v vorige maand)'
 huizenzoeker1$perc_ver_overboden<-gsub('[%]','', huizenzoeker1$perc_ver_overboden) 
 huizenzoeker1$perc_ver_overboden<-parse_number(huizenzoeker1$perc_ver_overboden)
 huizenzoeker1$perc_ver_overboden<-as.numeric(huizenzoeker1$perc_ver_overboden)
 
 #BESTEEDBAAR INKOMEN
-huizenzoeker1$best_inkomen<-gsub('[£á]','', huizenzoeker1$'Besteedbaar inkomen (per huishouden)') #remove euro signs
+huizenzoeker1$best_inkomen<-gsub('[â‚¬]','', huizenzoeker1$'Besteedbaar inkomen (per huishouden)') #remove euro signs
 huizenzoeker1$best_inkomen<-parse_number(huizenzoeker1$best_inkomen) #UPDATE: HERE NOW RETURNS ERROR: 3 PARSING FAILURES: because here are NA's
 huizenzoeker1$best_inkomen<-as.numeric(huizenzoeker1$best_inkomen)
 
@@ -78,7 +79,7 @@ huizenzoeker1$perc_pop_stijging<-as.numeric(huizenzoeker1$perc_pop_stijging)
 #Here we leave out the perc pop daling because it only contains NA's (not valuable)
 
 #now we remove the old columns that we don't need anymore
-huizenzoeker_province_data1<-huizenzoeker1%>%select(-c('Gem. vraagprijs','%£G Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%£G Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%£G M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%£G Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', 'Aantal inwoners', '% Populatie stijging','% Populatie daling'))
+huizenzoeker_province_data1<-huizenzoeker1%>%select(-c('Gem. vraagprijs','%Î” Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%Î” Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%Î” M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%Î” Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', 'Aantal inwoners', '% Populatie stijging'))
 
 #now we can explore the summary statistics for all our variables for the province Noord-Brabant
 View(huizenzoeker_province_data1)
@@ -86,4 +87,60 @@ summary(huizenzoeker_province_data1)
 #now convert the dataframe to a CSV file to import it into Jupyter to there see the summary statistics too
 write.csv(huizenzoeker_province_data1, "C:\\Users\\danie\\OneDrive\\Documents\\Repositories\\oDCM-project-team-3\\src\\collection\\huizenzoeker_province_data1.csv",row.names=FALSE)
 
-##Now we can use this dataset to visualise our scraped data and create graphs if we want. 
+##Now we can use this dataset to visualise our scraped data and create graphs if we want.
+
+gem_vraagprijs %>%
+  huizenzoeker_province_data1
+
+library(ggplot2)
+library(maps)
+library(viridis)
+
+NL <- map_data("world", region = "Netherlands")
+ggplot(NL, aes(x = long, y = lat)) +
+  geom_polygon(aes( group = group, fill = region))+
+  theme(legend.position = "none")
+
+library(sp)  # you need this library to work with SpatialPolygons
+NLD <- readRDS("gadm36_NLD_2_sp.rds")
+plot(NLD)
+
+library(tibble) # better printing
+library(dplyr)  # obviously
+NLD@data %>% as_tibble()
+NLD@data %>% tail(2)
+ggplot(data = NLD, aes(x = long, y = lat))+
+  geom_polygon( ) +
+  coord_fixed()
+ggplot()+
+  geom_polygon(data = NLD, aes(x = long, y = lat, group = group)) +
+  coord_map()
+
+
+NLD_fixed <- subset(NLD, !NLD$NAME_1  %in% c("Zeeuwse meren", "IJsselmeer"))
+NLD_fixed <- fortify(NLD_fixed)
+ggplot(NLD_fixed) +
+  geom_polygon( aes(x = long, y = lat, group = group))+
+  coord_map()
+ggplot(NLD_fixed) +
+  theme_minimal()+  # no backgroundcolor
+  geom_polygon( aes(x = long, y = lat, group = group),
+                color = "white",   # color is the lines of the region
+                fill = "#9C9797")+ # fill is the fill of every polygon.
+  coord_map()
+
+names_and_numbers <- data_frame(id=rownames(NLD@data),
+                                Province=NLD@data$NAME_2) %>% 
+  left_join(huizenzoeker_province_data1, by = "Province")
+
+final_map <- left_join(NLD_fixed, names_and_numbers, by = "id")
+final_map_na <- final_map %>%
+  filter(!is.na(Province))
+
+ggplot(final_map)+
+  theme_minimal()+
+  geom_polygon( aes(x = long, y = lat, group = group, fill= gem_vraagprijs)) +
+  coord_map()+
+  scale_fill_distiller(name = "Number of people in gemeente", # change titel legend
+                       palette = "Spectral")+ # change the color scheme
+  theme(legend.position = "bottom")  # chagne the legend position
