ca47902c9f888a2fdb766d72f3df08f355b0bda0
Daniëlle van Bruggen <d.m.vanbruggen@tilburguniversity.edu>
2021-10-17 13:46:08 +0200

    many updates for the final project

--- src/collection/Final source code/exploring_final_dataframe.R
+++ src/collection/Final source code/exploring_final_dataframe.R
@@ -1,151 +1,85 @@
-##ODCM project - team 3 - Huizenzoeker.nl
-##Script to make the scraped data work as a dataset: final_dataframe data
-setwd("C:/Users/danie/OneDrive/Documents/Repositories/oDCM-project-team-3/src/collection")
+#####################################################
+####Scraped municipality-level data Huizenzoeker#####
+#####################################################
 
-##loading the exported data from Jupyter Notebook
+# GOAL: MAKING OUR SCRAPED MUNICIPALITY DATA A FUNCTIONAL DATASET. 
+
+## Loading packages and data 
 library(readr)
 library(dplyr)
 huizenzoeker <- read_csv("huizenzoeker_scraper_data.csv")
-huizenzoeker <- huizenzoeker_scraper_data
+huizenzoeker<-huizenzoeker[2:15] #delete the first row with indices (not needed)
 View(huizenzoeker)
 summary(huizenzoeker)
 head(huizenzoeker)
-huizenzoeker<-huizenzoeker[2:15] #delete the first row with indices (not needed)
-#UPDATE: I changed the thousands delimiters in Python to , and in R this removes the thousand delimiter, I dont know if we should add it for clarity? but this already returns better summary statistics I think)
-#WARNING: the euro signs and delta's may change due to encoding erorrs when saving the file; if this happens, just replace the wierd signs with either the euro sign or the delta and youre fine
-#Therefore try to save the file with Big5 encoding (then there likely isn't an error)
 
-##checking classes variables
-#correcting the classes of the variables
-lapply(huizenzoeker, class) #initially they are all seen as characters, so we need to adjust this: 
-#QUADRANT 1
-#gemiddelde vraagprijs 
-huizenzoeker$gem_vraagprijs<-huizenzoeker$'Gem. vraagprijs'
-huizenzoeker$gem_vraagprijs<-gsub('[€]','', huizenzoeker$gem_vraagprijs)
-huizenzoeker$gem_vraagprijs<-parse_number(huizenzoeker$gem_vraagprijs) 
+## 1: Transforming data
+#Correcting classes
+sapply(huizenzoeker,class) #most of the columns' values are seen as characters, while they should be numeric. 
+
+huizenzoeker$province<-huizenzoeker$Province
+huizenzoeker$city<-huizenzoeker$City
+
+# QUADRANT 1
+#Gemiddelde vraagprijs column
+huizenzoeker$gem_vraagprijs<-gsub('[€]','', huizenzoeker$'Gem. vraagprijs') #rename column and remove euro signs
+huizenzoeker$gem_vraagprijs<-parse_number(huizenzoeker$gem_vraagprijs) #make values numeric
 huizenzoeker$gem_vraagprijs<-as.numeric(huizenzoeker$gem_vraagprijs)
-#verandering 
-huizenzoeker$perc_ver_vraagprijs<-huizenzoeker$'%Δ Vraagprijs (t.o.v vorige maand)'
-huizenzoeker$perc_ver_vraagprijs<-gsub('[%]','',huizenzoeker$perc_ver_vraagprijs)
-huizenzoeker$perc_ver_vraagprijs<-parse_number(huizenzoeker$perc_ver_vraagprijs)
+#Perc verandering vraagprijs column 
+huizenzoeker$perc_ver_vraagprijs<-gsub('[%]','',huizenzoeker$'%Δ Vraagprijs (t.o.v vorige maand)') #rename column and remove %
+huizenzoeker$perc_ver_vraagprijs<-parse_number(huizenzoeker$perc_ver_vraagprijs) #make values numeric
 huizenzoeker$perc_ver_vraagprijs<-as.numeric(huizenzoeker$perc_ver_vraagprijs)
 
-#QUADRANT 2
-#verkochte woningen
-huizenzoeker$verk_woningen<-huizenzoeker$'Verkochte woningen'
+# QUADRANT 2
+#Verkochte woningen column
+huizenzoeker$verk_woningen<-huizenzoeker$'Verkochte woningen' 
 huizenzoeker$verk_woningen<-as.numeric(huizenzoeker$verk_woningen)
-#verandering
-huizenzoeker$perc_ver_verkocht<-huizenzoeker$'%Δ Verkochte woningen (t.o.v vorige maand)'
-huizenzoeker$perc_ver_verkocht<-gsub('[%]','',huizenzoeker$perc_ver_verkocht)
+#Perc verandering verkochte woningen column 
+huizenzoeker$perc_ver_verkocht<-gsub('[%]','',huizenzoeker$'%Δ Verkochte woningen (t.o.v vorige maand)')
 huizenzoeker$perc_ver_verkocht<-parse_number(huizenzoeker$perc_ver_verkocht)
 huizenzoeker$perc_ver_verkocht<-as.numeric(huizenzoeker$perc_ver_verkocht)
 
-#QUADRANT 3
-#gemiddelde m2 prijs 
-huizenzoeker$gem_m2prijs<-gsub('[€]','', huizenzoeker$`Gem. m2 prijs`) #remove euro signs
+# QUADRANT 3
+#Gemiddelde m2 prijs column
+huizenzoeker$gem_m2prijs<-gsub('[€]','', huizenzoeker$'Gem. m2 prijs') 
 huizenzoeker$gem_m2prijs<-parse_number(huizenzoeker$gem_m2prijs)
 huizenzoeker$gem_m2prijs<-as.numeric(huizenzoeker$gem_m2prijs)
-#verandering
-huizenzoeker$perc_ver_m2prijs<-huizenzoeker$'%Δ M2 prijs (t.o.v vorige maand)'
-huizenzoeker$perc_ver_m2prijs<-gsub('[%]','',huizenzoeker$perc_ver_m2prijs)
+#Perc verandering gemiddelde m2 prijs column
+huizenzoeker$perc_ver_m2prijs<-gsub('[%]','',huizenzoeker$'%Δ M2 prijs (t.o.v vorige maand)')
 huizenzoeker$perc_ver_m2prijs<-parse_number(huizenzoeker$perc_ver_m2prijs)
 huizenzoeker$perc_ver_m2prijs<-as.numeric(huizenzoeker$perc_ver_m2prijs)
 
-#QUADRANT 4
-#percentage overboden
-huizenzoeker$perc_overboden<-gsub('[%]','', huizenzoeker$`% Vraagprijs overboden`) 
+# QUADRANT 4
+#Gemiddelde percentage overboden column 
+huizenzoeker$perc_overboden<-gsub('[%]','', huizenzoeker$'% Vraagprijs overboden') 
 huizenzoeker$perc_overboden<-parse_number(huizenzoeker$perc_overboden)
 huizenzoeker$perc_overboden<-as.numeric(huizenzoeker$perc_overboden)
-#verandering
-huizenzoeker$perc_ver_overboden<-huizenzoeker$'%Δ Overboden (t.o.v vorige maand)'
-huizenzoeker$perc_ver_overboden<-gsub('[%]','', huizenzoeker$perc_ver_overboden) 
+#Perc verandering percentage overboden column 
+huizenzoeker$perc_ver_overboden<-gsub('[%]','', huizenzoeker$'%Δ Overboden (t.o.v vorige maand)') 
 huizenzoeker$perc_ver_overboden<-parse_number(huizenzoeker$perc_ver_overboden)
 huizenzoeker$perc_ver_overboden<-as.numeric(huizenzoeker$perc_ver_overboden)
 
-#BESTEEDBAAR INKOMEN
-huizenzoeker$best_inkomen<-gsub('[€ ]','', huizenzoeker$'Besteedbaar inkomen (per huishouden)') #remove euro signs
-huizenzoeker$best_inkomen<-parse_number(huizenzoeker$best_inkomen) #UPDATE: HERE NOW RETURNS ERROR: 3 PARSING FAILURES: because here are NA's
+# BESTEEDBAAR INKOMEN
+huizenzoeker$best_inkomen<-gsub('[€ ]','', huizenzoeker$'Besteedbaar inkomen (per huishouden)') 
+huizenzoeker$best_inkomen<-parse_number(huizenzoeker$best_inkomen) 
 huizenzoeker$best_inkomen<-as.numeric(huizenzoeker$best_inkomen)
 
-#INWONERS
-#aantal inwoners
+# INWONERS
+#Aantal inwoners column 
 huizenzoeker$n_inwoners<-huizenzoeker$'Aantal inwoners'
-#populatie stijging/daling
-huizenzoeker$perc_pop_stijging<-huizenzoeker$'% Populatie stijging'
-huizenzoeker$perc_pop_stijging<-gsub('[%]','', huizenzoeker$perc_pop_stijging) 
+#Perc populatie stijging of daling columns 
+huizenzoeker$perc_pop_stijging<-gsub('[%]','', huizenzoeker$'% Populatie stijging') 
 huizenzoeker$perc_pop_stijging<-parse_number(huizenzoeker$perc_pop_stijging)
 huizenzoeker$perc_pop_stijging<-as.numeric(huizenzoeker$perc_pop_stijging)
-huizenzoeker$perc_pop_daling<-huizenzoeker$'% Populatie daling'
-huizenzoeker$perc_pop_daling<-gsub('[%]','', huizenzoeker$perc_pop_daling) 
+huizenzoeker$perc_pop_daling<-gsub('[%]','', huizenzoeker$'% Populatie daling') 
 huizenzoeker$perc_pop_daling<-parse_number(huizenzoeker$perc_pop_daling)
 huizenzoeker$perc_pop_daling<-as.numeric(huizenzoeker$perc_pop_daling)
 
-#now we remove the old columns that we don't need anymore
-huizenzoeker_data2<-huizenzoeker%>%select(-c('Gem. vraagprijs','%Δ Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%Δ Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%Δ M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%Δ Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', '% Populatie stijging','% Populatie daling', 'Aantal inwoners'))
-
-#now we can explore the summary statistics for all our variables for the province Noord-Brabant
+#Remove the old unnecessary columns
+huizenzoeker_data2<-huizenzoeker%>%select(-c('Province', 'City', 'Gem. vraagprijs','%Δ Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%Δ Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%Δ M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%Δ Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', '% Populatie stijging','% Populatie daling', 'Aantal inwoners'))
+#Explore summary statistics dataset
 View(huizenzoeker_data2)
 summary(huizenzoeker_data2)
-#REMARKABLE FINDINGS (STILL TO SOLVE MAYBE?):
-#Eemsdelta in groningen has no inwoners, inwoners = 0 ?? strange. 
-#Why are there some NA's in besteedbaar_inkomen, is it not given for some municipalities? 
 
-#now convert the dataframe to a CSV file to import it into Jupyter to there see the summary statistics too
-write.csv(huizenzoeker_data2, "C:\\Users\\danie\\OneDrive\\Documents\\Repositories\\oDCM-project-team-3\\src\\collection\\huizenzoeker_data2.csv",row.names=FALSE)
-
-
-##### Provinces
-##Now we can use this dataset to visualise our scraped data and create graphs if we want. 
-library(ggplot2)
-library(maps)
-library(viridis)
-library(tibble) 
-library(sp)
-library(extrafont)
-
-NL <- map_data("world", region = "Netherlands")
-ggplot(NL, aes(x = long, y = lat)) +
-  geom_polygon(aes( group = group, fill = region))+
-  theme(legend.position = "none")
-
-NLD <- readRDS("gadm36_NLD_1_sp (1).rds")
-NLD_data <- NLD@data
-plot(NLD)
-
-ggplot(data = NLD, aes(x = long, y = lat))+
-  geom_polygon( ) +
-  coord_fixed()
-ggplot()+
-  geom_polygon(data = NLD, aes(x = long, y = lat, group = group)) +
-  coord_map()
-
-NLD_fixed <- subset(NLD, !NLD$NAME_1  %in% c("Veldhoven"))
-NLD_f<- fortify(NLD)
-
-names_and_numbers <- data_frame(id=rownames(NLD_data),
-                                Province=NLD_data$NAME_1) %>% 
-  left_join(huizenzoeker_province_data1, by = "Province")
-
-final_map <- left_join(NLD_f, names_and_numbers, by = "id") 
-
-ggplot(final_map) +
-  theme_minimal() +
-  geom_polygon(aes(x = long, y = lat, group = group, fill= perc_overboden)) +
-  labs(x="", y="", title="Dutch Housing Crisis: Which Areas Are Most Outbid?",
-       subtitle = "Concerning Provinces in the Netherlands") +
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.title.y=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank(),
-        legend.position = "bottom") +
-  coord_map()+
-  scale_fill_distiller(name = "% outbid", 
-                       palette = "Reds",
-                       direction = 1) +
-  theme(legend.position = "right",
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank()) 
-
-
+#Convert dataset to CSV (to import into Jupyter Notebooks)
+write.csv(huizenzoeker_data2, "huizenzoeker_municipality.csv",row.names=FALSE)
--- src/collection/Final source code/exploring_province_dataframe.R
+++ src/collection/Final source code/exploring_province_dataframe.R
@@ -1,114 +1,108 @@
-##ODCM project - team 3 - Huizenzoeker.nl
-##Script to make the scraped data work as a dataset: province_dataframe
-setwd("C:/Users/danie/OneDrive/Documents/Repositories/ODCM-project-team-3/src/collection")
+#####################################################
+####Scraped province-level data Huizenzoeker#########
+#####################################################
 
-##loading the exported data from Jupyter Notebook
+# GOAL: MAKING OUR SCRAPED PROVICNE DATA A FUNCTIONAL DATASET. 
+
+## Loading packages and data 
 library(readr)
 library(dplyr)
 huizenzoeker1 <- read_csv("huizenzoeker_province_data.csv")
-huizenzoeker1 <- huizenzoeker_province_data
+huizenzoeker1<-huizenzoeker1[2:14] #delete the first row with indices (not needed)
 View(huizenzoeker1)
 summary(huizenzoeker1)
 head(huizenzoeker1)
-huizenzoeker1<-huizenzoeker1[2:13] #delete the first row with indices (not needed)
-#UPDATE: I changed the thousands delimiters in Python to , and in R this removes the thousand delimiter, I dont know if we should add it for clarity? but this already returns better summary statistics I think)
-#SAME WARNING HERE: SAVE WITH BIG5 ENCODING SUCH THAT EURO SIGNS AND DELTA'S DON'T GET MIXED UP
-#REMARKABLE: %POPULATIE DALING IS EMPTY, AS FOR EVERY PROVINCE THE NUMBER OF INHABITANTS HAS INCREASED (so we can get rid of this column)
 
+## 1: Transforming data
+#Correcting classes
+sapply(huizenzoeker1,class) #most of the columns' values are seen as characters, while they should be numeric. 
 
-##checking classes variables
-#correcting the classes of the variables
-lapply(huizenzoeker1, class) #initially they are all seen as characters, so we need to adjust this: 
-#QUADRANT 1
-#gemiddelde vraagprijs 
-huizenzoeker1$gem_vraagprijs<-huizenzoeker1$'Gem. vraagprijs'
-huizenzoeker1$gem_vraagprijs<-gsub('[€]','', huizenzoeker1$gem_vraagprijs) #remove euro signs
-huizenzoeker1$gem_vraagprijs<-parse_number(huizenzoeker1$gem_vraagprijs) #PROBLEM: 1 average above 1 million, is seen as 2000 instead of 2,000,000!! 
+# QUADRANT 1
+#Gemiddelde vraagprijs column 
+huizenzoeker1$gem_vraagprijs<-gsub('[€]','', huizenzoeker1$'Gem. vraagprijs') 
+huizenzoeker1$gem_vraagprijs<-parse_number(huizenzoeker1$gem_vraagprijs) 
 huizenzoeker1$gem_vraagprijs<-as.numeric(huizenzoeker1$gem_vraagprijs)
-#verandering 
-huizenzoeker1$perc_ver_vraagprijs<-huizenzoeker1$'%Δ Vraagprijs (t.o.v vorige maand)'
-huizenzoeker1$perc_ver_vraagprijs<-gsub('[%]','',huizenzoeker1$perc_ver_vraagprijs)
+#Perc verandering vraagprijs column
+huizenzoeker1$perc_ver_vraagprijs<-gsub('[%]','',huizenzoeker1$'%Δ Vraagprijs (t.o.v vorige maand)')
 huizenzoeker1$perc_ver_vraagprijs<-parse_number(huizenzoeker1$perc_ver_vraagprijs)
 huizenzoeker1$perc_ver_vraagprijs<-as.numeric(huizenzoeker1$perc_ver_vraagprijs)
 
-#QUADRANT 2
-#verkochte woningen
+# QUADRANT 2
+#Verkochte woningen column 
 huizenzoeker1$verk_woningen<-huizenzoeker1$'Verkochte woningen'
 huizenzoeker1$verk_woningen<-as.numeric(huizenzoeker1$verk_woningen)
-#verandering
-huizenzoeker1$perc_ver_verkocht<-huizenzoeker1$'%Δ Verkochte woningen (t.o.v vorige maand)'
-huizenzoeker1$perc_ver_verkocht<-gsub('[%]','',huizenzoeker1$perc_ver_verkocht)
+#Perc verandering verkochte woningen column
+huizenzoeker1$perc_ver_verkocht<-gsub('[%]','',huizenzoeker1$'%Δ Verkochte woningen (t.o.v vorige maand)')
 huizenzoeker1$perc_ver_verkocht<-parse_number(huizenzoeker1$perc_ver_verkocht)
 huizenzoeker1$perc_ver_verkocht<-as.numeric(huizenzoeker1$perc_ver_verkocht)
 
-#QUADRANT 3
-#gemiddelde m2 prijs 
-huizenzoeker1$gem_m2prijs<-gsub('[€]','', huizenzoeker1$`Gem. m2 prijs`) #remove euro signs
+# QUADRANT 3
+#Gemiddelde m2 prijs column 
+huizenzoeker1$gem_m2prijs<-gsub('[€]','', huizenzoeker1$'Gem. m2 prijs')
 huizenzoeker1$gem_m2prijs<-parse_number(huizenzoeker1$gem_m2prijs)
 huizenzoeker1$gem_m2prijs<-as.numeric(huizenzoeker1$gem_m2prijs)
-#verandering
-huizenzoeker1$perc_ver_m2prijs<-huizenzoeker1$'%Δ M2 prijs (t.o.v vorige maand)'
-huizenzoeker1$perc_ver_m2prijs<-gsub('[%]','',huizenzoeker1$perc_ver_m2prijs)
+#Perc verandering gemiddelde m2 prijs column 
+huizenzoeker1$perc_ver_m2prijs<-gsub('[%]','',huizenzoeker1$'%Δ M2 prijs (t.o.v vorige maand)')
 huizenzoeker1$perc_ver_m2prijs<-parse_number(huizenzoeker1$perc_ver_m2prijs)
 huizenzoeker1$perc_ver_m2prijs<-as.numeric(huizenzoeker1$perc_ver_m2prijs)
 
-#QUADRANT 4
-#percentage overboden
-huizenzoeker1$perc_overboden<-gsub('[%]','', huizenzoeker1$`% Vraagprijs overboden`) 
+# QUADRANT 4
+#Gemiddeld percentage overboden column
+huizenzoeker1$perc_overboden<-gsub('[%]','', huizenzoeker1$'% Vraagprijs overboden') 
 huizenzoeker1$perc_overboden<-parse_number(huizenzoeker1$perc_overboden)
 huizenzoeker1$perc_overboden<-as.numeric(huizenzoeker1$perc_overboden)
-#verandering
-huizenzoeker1$perc_ver_overboden<-huizenzoeker1$'%Δ Overboden (t.o.v vorige maand)'
-huizenzoeker1$perc_ver_overboden<-gsub('[%]','', huizenzoeker1$perc_ver_overboden) 
+#Perc verandering percentage overboden column
+huizenzoeker1$perc_ver_overboden<-gsub('[%]','', huizenzoeker1$'%Δ Overboden (t.o.v vorige maand)') 
 huizenzoeker1$perc_ver_overboden<-parse_number(huizenzoeker1$perc_ver_overboden)
 huizenzoeker1$perc_ver_overboden<-as.numeric(huizenzoeker1$perc_ver_overboden)
 
-#BESTEEDBAAR INKOMEN
-huizenzoeker1$best_inkomen<-gsub('[€]','', huizenzoeker1$'Besteedbaar inkomen (per huishouden)') #remove euro signs
-huizenzoeker1$best_inkomen<-parse_number(huizenzoeker1$best_inkomen) #UPDATE: HERE NOW RETURNS ERROR: 3 PARSING FAILURES: because here are NA's
+# BESTEEDBAAR INKOMEN
+huizenzoeker1$best_inkomen<-gsub('[€]','', huizenzoeker1$'Besteedbaar inkomen (per huishouden)')
+huizenzoeker1$best_inkomen<-parse_number(huizenzoeker1$best_inkomen) 
 huizenzoeker1$best_inkomen<-as.numeric(huizenzoeker1$best_inkomen)
 
-#INWONERS
-#aantal inwoners
+# INWONERS
+#Aantal inwoners column
 huizenzoeker1$n_inwoners<-huizenzoeker1$'Aantal inwoners'
-#populatie stijging/daling
-huizenzoeker1$perc_pop_stijging<-huizenzoeker1$'% Populatie stijging'
-huizenzoeker1$perc_pop_stijging<-gsub('[%]','', huizenzoeker1$perc_pop_stijging) 
+#Perc populatie stijging of daling columns
+huizenzoeker1$perc_pop_stijging<-gsub('[%]','', huizenzoeker1$'% Populatie stijging') 
 huizenzoeker1$perc_pop_stijging<-parse_number(huizenzoeker1$perc_pop_stijging)
 huizenzoeker1$perc_pop_stijging<-as.numeric(huizenzoeker1$perc_pop_stijging)
-#Here we leave out the perc pop daling because it only contains NA's (not valuable)
+huizenzoeker1$perc_pop_daling<-gsub('[%]','', huizenzoeker1$'% Populatie daling') 
+huizenzoeker1$perc_pop_daling<-parse_number(huizenzoeker1$perc_pop_daling)
+huizenzoeker1$perc_pop_daling<-as.numeric(huizenzoeker1$perc_pop_daling)
 
-#now we remove the old columns that we don't need anymore
-huizenzoeker_province_data1<-huizenzoeker1%>%select(-c('Gem. vraagprijs','%Δ Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%Δ Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%Δ M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%Δ Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', 'Aantal inwoners', '% Populatie stijging'))
-
-#now we can explore the summary statistics for all our variables for the province Noord-Brabant
+#Remove the old unnecessary columns
+huizenzoeker_province_data1<-huizenzoeker1%>%select(-c('Gem. vraagprijs','%Δ Vraagprijs (t.o.v vorige maand)', 'Verkochte woningen','%Δ Verkochte woningen (t.o.v vorige maand)','Gem. m2 prijs','%Δ M2 prijs (t.o.v vorige maand)','% Vraagprijs overboden','%Δ Overboden (t.o.v vorige maand)','Besteedbaar inkomen (per huishouden)', 'Aantal inwoners', '% Populatie stijging', '% Populatie daling'))
+#Explore summary statistics dataset
 View(huizenzoeker_province_data1)
 summary(huizenzoeker_province_data1)
-#now convert the dataframe to a CSV file to import it into Jupyter to there see the summary statistics too
-write.csv(huizenzoeker_province_data1, "C:\\Users\\danie\\OneDrive\\Documents\\Repositories\\oDCM-project-team-3\\src\\collection\\huizenzoeker_province_data1.csv",row.names=FALSE)
 
-##Now we can use this dataset to visualise our scraped data and create graphs if we want.
+#Convert dataset to CSV (to import into Jupyter Notebooks)
+write.csv(huizenzoeker_province_data1, "huizenzoeker_province.csv",row.names=FALSE)
 
-gem_vraagprijs %>%
-  huizenzoeker_province_data1
 
+## 2: Visualisations data
+
+#Loading packages
 library(ggplot2)
 library(maps)
 library(viridis)
+library(tibble) #for better printing properties
+library(sp) #needed to work with SpatialPolygons
+library(mapproj)
+library(extrafont)
 
+#Visualisations of the average % outbid for each province in the Netherlands
 NL <- map_data("world", region = "Netherlands")
 ggplot(NL, aes(x = long, y = lat)) +
   geom_polygon(aes( group = group, fill = region))+
   theme(legend.position = "none")
 
-library(sp)  # you need this library to work with SpatialPolygons
 NLD <- readRDS("gadm36_NLD_2_sp.rds")
+NLD_data <- NLD@data
 plot(NLD)
 
-library(tibble) # better printing
-library(dplyr)  # obviously
-NLD@data %>% as_tibble()
-NLD@data %>% tail(2)
 ggplot(data = NLD, aes(x = long, y = lat))+
   geom_polygon( ) +
   coord_fixed()
@@ -116,31 +110,39 @@ ggplot()+
   geom_polygon(data = NLD, aes(x = long, y = lat, group = group)) +
   coord_map()
 
+NLD_fixed <- subset(NLD, !NLD$NAME_1  %in% c("Veldhoven"))
+NLD_f<- fortify(NLD)
 
-NLD_fixed <- subset(NLD, !NLD$NAME_1  %in% c("Zeeuwse meren", "IJsselmeer"))
-NLD_fixed <- fortify(NLD_fixed)
-ggplot(NLD_fixed) +
-  geom_polygon( aes(x = long, y = lat, group = group))+
-  coord_map()
-ggplot(NLD_fixed) +
-  theme_minimal()+  # no backgroundcolor
-  geom_polygon( aes(x = long, y = lat, group = group),
-                color = "white",   # color is the lines of the region
-                fill = "#9C9797")+ # fill is the fill of every polygon.
-  coord_map()
-
-names_and_numbers <- data_frame(id=rownames(NLD@data),
-                                Province=NLD@data$NAME_2) %>% 
+names_and_numbers <- data_frame(id=rownames(NLD_data),
+                                Province=NLD_data$NAME_1) %>% 
   left_join(huizenzoeker_province_data1, by = "Province")
 
-final_map <- left_join(NLD_fixed, names_and_numbers, by = "id")
-final_map_na <- final_map %>%
-  filter(!is.na(Province))
+final_map <- left_join(NLD_f, names_and_numbers, by = "id") 
 
-ggplot(final_map)+
-  theme_minimal()+
-  geom_polygon( aes(x = long, y = lat, group = group, fill= gem_vraagprijs)) +
+ggplot(final_map) +
+  theme_minimal() +
+  geom_polygon(aes(x = long, y = lat, group = group, fill= perc_overboden)) +
+  labs(x="", y="", title="Dutch Housing Crisis: Which Areas Are Most Outbid?",
+       subtitle = "Concerning Provinces in the Netherlands") +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        legend.position = "bottom") +
   coord_map()+
-  scale_fill_distiller(name = "Number of people in gemeente", # change titel legend
-                       palette = "Spectral")+ # change the color scheme
-  theme(legend.position = "bottom")  # chagne the legend position
+  scale_fill_distiller(name = "% outbid", 
+                       palette = "Reds",
+                       direction = 1) +
+  theme(legend.position = "right",
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank()) 
+
+
+
+
+
+
+
+
